0a2d1d2c4c7e6f9ee402caf2cf4ba816c3e3c8a2
杨攀 <yangpan@baixing.com>
2018-01-17 15:49:38 +0800

    first commit

--- procfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = (pandora) => {
- 
-  if(pandora.dev) {
-    pandora.process('worker').nodeArgs(['-r', 'ts-node/register', '--trace-warnings'])
-    pandora.service('dashboard', './src/index').process('worker')
-  } else {
-    pandora.service('dashboard', './dist/index').process('worker')
-  }
-};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 
+import 'reflect-metadata'
 import * as Koa from 'koa'
+import * as Router from 'koa-router'
+import {createServer} from './router/index'
 const app = new Koa()
-const port = 8080
 
-app.use(async ctx => {
-    ctx.body = "It not works!\n"
-})
+const router = new Router()
+
+const port = 9000
+
+createServer(app, router,[__dirname + "/controller/**"]);
 
 app.listen(port,()=>{
     console.log(`locahost:${port}`)
