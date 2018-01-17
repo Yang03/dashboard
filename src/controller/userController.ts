5e954ad95575066b6a3a033d228240877c726326
杨攀 <yangpan@baixing.com>
2018-01-17 15:53:24 +0800

    add decorators

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -2,9 +2,9 @@ import { Controller, Route } from "../router/index";
 import * as Koa from 'koa';
 
 @Controller('user')
-export default class userController {
+export default class UserController {
     @Route('get', 'test')
-        test(ctx: Koa.Context) {
+    test(ctx: Koa.Context) {
         ctx.body = 'test'
     }
 }
