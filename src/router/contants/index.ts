a8ed2d30bed890f8c53d0b7c419f11362feb813f
杨攀 <yangpan@baixing.com>
2018-01-22 11:40:05 +0800

    add method decorator

---
+++ src/router/contants/index.ts
@@ -0,0 +1,10 @@
+export const METHODS = {
+    HEAD: 'head',
+    GET: 'get',
+    POST: 'post',
+    PUT: 'put',
+    PATCH: 'patch',
+    DELETE: 'delete',
+    OPTIONS: 'options',
+    ALL: 'all'
+}
